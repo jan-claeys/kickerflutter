83d823838ae8764f32406d00e15cd85bab476493
AzureAD\JanClaeys <jan.claeys@tillit.be>
2023-12-10 23:37:48 +0100

    Add logout functionality

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kickerflutter/network.dart';
+import 'package:kickerflutter/pages/login_page.dart';
 
 import '../models/player.dart';
 import '../session.dart';
@@ -18,18 +19,22 @@ class ProfilePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FutureBuilder<Player?>(
-              future: fetchCurrentPlayer(),
-              builder: (BuildContext context, AsyncSnapshot<Player?>snapshot) => snapshot.hasData
-                  ? Text(
-                      snapshot.data!.name,
-                      style: const TextStyle(fontSize: 24),
-                    )
-                  : const CircularProgressIndicator()
-            ),
+                future: fetchCurrentPlayer(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<Player?> snapshot) =>
+                        snapshot.hasData
+                            ? Text(
+                                snapshot.data!.name,
+                                style: const TextStyle(fontSize: 24),
+                              )
+                            : const CircularProgressIndicator()),
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                // TODO: Implement logout functionality
+                Session().logout();
+                Navigator.pop(context);
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()));
               },
               child: const Text('Logout'),
             ),
